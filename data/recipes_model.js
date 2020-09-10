c81297b246b867d52cfde2822a9d68cf2a1a054e
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-09-10 19:44:26 -0400

    grrrr

--- data/recipes_model.js
+++ data/recipes_model.js
@@ -1,25 +1,42 @@
 const db = require('./data/config')
 
-function getRecipes() {
+async function find() {
+  return await db('recipes')
+}
+
+function findById(id) {
   return db('recipes')
+    .where('recipes.id', id)
+    .select('recipes.name')
 }
 
-function getInstructions(recipe_id) {
-  return db('steps')
-    .where('steps.recipe_id')
+function findInstructions(id) {
+  return db('recipes')
+    .where('recipes.id', id)
+    .join('steps', 'steps.recipes_id', 'recipes.id')
+    .select('steps.description as instruction', 'steps.step as stepOrder')
+    .orderBy('steps.step', 'asc')
 }
 
-function getShoppingList(recipe_id) {
+function findIngredients(id) {
+  return db('recipes_ingredients')
+    .where('recipes_ingredients.recipe_id', id)
+    .join('ingredients', 'ingredients.id', 'recipes_ingredients.ingredient_id')
+    .select('ingredients.name as ingredient')
+}
+
+function findRecipesByIngredient(id) {
   return db('ingredients')
-  
+    .where('ingredients.id', id)
+    .join('recipes_ingredients', 'ingredients.id', 'recipes_ingredients.ingredient_id')
+    .join('recipes', 'recipes.id', 'recipes_ingredients.recipe_id')
+    .select('recipes.name as recipe')
 }
 
 module.exports = {
-  getRecipes,
-  getShoppingList,
-  getInstructions,
+  find,
+  findById,
+  findInstructions,
+  findIngredients,
+  findRecipesByIngredient,
 }
-
-// - `getRecipes()`: should return a list of all recipes in the database.
-// - `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
-// - `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
--- data/seeds/04-quantity.js
+++
@@ -1,6 +0,0 @@
-exports.seed = function(knex) {
-  return knex('quantity').insert([
-    {amount: '1 cup'},
-    {amount: '1 teaspoon'},
-  ])
-};
